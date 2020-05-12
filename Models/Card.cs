faaf96980c701c45aa7522640625b4bab0a5fde9
Jim Greaves <greavesjim3@gmail.com>
2020-05-12 10:29:04 -0600

    first commit

---
+++ Models/Card.cs
@@ -0,0 +1,18 @@
+namespace blackjack.Models
+{
+  class Card{
+public string Name {get; set;}
+
+public int Value {get; set;}
+
+
+public Card(string name, int value){
+
+Name = name;
+Value = value;
+
+}
+
+
+  } 
+}
---
+++ Models/Game.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+
+namespace blackjack.Models
+{
+  class Game
+  {
+
+
+
+    public List<Card> Cards { get; set; } = new List<Card>();
+
+    public List<Card> PlayerCards { get; set; } = new List<Card>();
+
+    public List<Card> CompCards { get; set; } = new List<Card>();
+
+    public void addCard(Card x)
+    {
+      Cards.Add(x);
+    }
+
+    public void DealCards()
+    {
+
+      for (int i = 0; i < 2; i++)
+      {
+
+        Random rng = new Random();
+        int index = rng.Next(Cards.Count);
+        Card Dealt = Cards[index];
+        PlayerCards.Add(Dealt);
+        Cards.RemoveAt(index);
+      }
+      for (int i = 0; i < 2; i++)
+      {
+
+        Random rng = new Random();
+        int index = rng.Next(Cards.Count);
+        Card Dealt = Cards[index];
+        CompCards.Add(Dealt);
+        Cards.RemoveAt(index);
+      }
+
+    }
+    public void PrintCards()
+    {
+
+      Console.WriteLine($"The Computer has {CompCards[0].Name}");
+      int score = 0;
+
+      for (int i = 0; i < PlayerCards.Count; i++)
+      {
+        score += PlayerCards[i].Value;
+        Console.WriteLine($"You have {CompCards[0].Name}");
+        Console.WriteLine($"Your total is {score}")
+      }
+
+
+
+    }
+    public void Hit()
+    {
+
+
+
+
+    }
+
+    public void Validate()
+    {
+
+    }
+
+  }
+}
